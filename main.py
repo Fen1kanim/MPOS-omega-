2bffe8e64cc389ca8a29e93ce1856579b2d9b907
Fen1k <fen1k.1234567@gmail.com>
2025-11-27 15:53:54 +0100

    added json integration

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import datetime
 import os
+import json
 
-# keywords
-hi = ['hi', 'hello', 'hallo', 'h']
-time = ['time', 'which time it is?', 'which time is it?', 'which time is it', 'which time it is', 'time?']
-date = ['date', 'which date it is?', 'which date is it?', 'which date is it', 'which date it is', 'date?']
-firefox = ['fire', 'firefox', 'open firefox', 'please open firefox']
-steam = ['steam', 'open steam', 'please open steam']
+with open("keywords", "r") as jf:
+    js = json.load(jf)
 
 while True: # main cycle
     stdin = input("--> ")
-    [print('Hi! How r u?') for i in hi if stdin == i] # hi
-    [print('It is', datetime.datetime.now().strftime("%H:%M")) for i in time if stdin == i] # time
-    [print('Today is', datetime.datetime.now().strftime("%d %B of %y")) for i in date if stdin == i] # date
-    [[print('No problem!'), os.system('firefox')] for i in firefox if stdin == i] # open firefox
-    [[print('No problem!'), os.system('steam')] for i in steam if stdin == i] # open steam
+    [print('Hi! How r u?') for i in jf["hi"] if stdin == i] # hi
+    [print('It is', datetime.datetime.now().strftime("%H:%M")) for i in js["time"] if stdin == i] # time
+    [print('Today is', datetime.datetime.now().strftime("%d %B of %y")) for i in js["date"] if stdin == i] # date
+    [[print('No problem!'), os.system('firefox')] for i in js["firefox"] if stdin == i] # open firefox
+    [[print('No problem!'), os.system('steam')] for i in js["steam"] if stdin == i] # open steam
     [os.system('clear') if stdin == 'clear' or stdin == 'cls' else None] # clear terminal
     if stdin == 'exit': # exit to terminal
         break
