352d5681618e3a2fc63985bfc23e0edf1eedee61
Fen1k <fen1k.1234567@gmail.com>
2025-11-28 21:53:57 +0100

    added the help keyword

---
+++ help.py
@@ -0,0 +1,11 @@
+print('''keywords:
+date - show date
+time - show time
+fire - open firefox
+steam - open steam
+calc - open simple calculator
+exit - exit
+clear, cls - clear terminal
+help - list commands, you use it right now
+hi - just say hi! Be polite, i spend 3 days for this shit''')
+print()
