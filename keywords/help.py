5cc3b19954b92c8fcff582a5f8d56a8c85355452
Fen1k <fen1k.1234567@gmail.com>
2025-12-08 16:22:36 +0100

    moved help.py to the keywords directory

---
+++ keywords/help.py
@@ -0,0 +1,12 @@
+print('''keywords:
+date - show date
+time - show time
+calc - open simple calculator
+exit - exit
+clear, cls - clear terminal
+help - list commands, you use it right now
+hi - just say hi! Be polite, i spend around 2 weeks for this shit
+open - types a command to terminal
+whoami - shows you how are you authentificated as
+delete - delete youre account''')
+print()
