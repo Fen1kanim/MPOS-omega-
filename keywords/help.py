680e8c5bdb572f6f6a76fb698b9de81b3dfe58e9
Fen1k <fen1k.1234567@gmail.com>
2025-12-10 16:48:38 +0100

    added game command description. deleted doom guess and pong descriptions

--- keywords/help.py
+++ keywords/help.py
@@ -10,9 +10,5 @@ open - types a command to terminal
 whoami - shows you how are you authentificated as
 delete - delete youre account
 uranium - irradiates you
-pong - play pong alone or with your friend. it has two more variables after pong and they are:
-1. points to win
-2. paddle lenght
-for ex. pong 10 6 will make the points to win to 10 and the paddle lenght to 6
-doom - play a simple doom like game''')
+game - opens game menu''')
 print()
