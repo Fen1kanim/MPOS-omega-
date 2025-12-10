cab9232ce5015c58e6a6286d7fc67341ff1077d6
Kirillpro3000 <gricajkirill59@gmail.com>
2025-12-10 16:41:18 +0100

    added rock paper scissors, snake game and tictactoe

--- keywords/games/menu.py
+++ keywords/games/menu.py
@@ -4,12 +4,18 @@ stdin = input('''
 1. 2.5D labyrinth
 2. guess a random number
 3. pong
+4. rock paper scissors
+5. snake
+6. tic tac toe
 
-choose a game(123): ''')
+choose a game(123456): ''')
 
 [call(["python", "./keywords/games/labyrinth.py"]) if stdin == "1" else None]
 [call(["python", "./keywords/games/guess.py"]) if stdin == '2' else None]
 [call(["python", "./keywords/games/pong.py"]) if stdin == '3' else None]
-if stdin != '1' or stdin != '2' or stdin != '3':
+[call(["python", "./keywords/games/rockpaperscissors.py"]) if stdin == '4' else None]
+[call(["python", "./keywords/games/snake.py"]) if stdin == '5' else None]
+[call(["python", "./keywords/games/tictactoe.py"]) if stdin == '6' else None]
+if stdin != '1' or stdin != '2' or stdin != '3' or stdin != '4' or stdin != '5' or stdin != '6':
     print('''
 try again''')
